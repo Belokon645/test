a598a5131ec10edd29185ddf0f8d662c2a8b7c69
Aleksander Belokon <coffeine645@gmail.com>
2021-08-17 13:32:13 +0300

    add some comments

--- test/Source.cpp
+++ test/Source.cpp
@@ -10,19 +10,16 @@ int main()
 	
 	//create greeteng string
 	const std::string greeting = "Hello, " + name + "!";
-
+	//set spaces here
 	const int pad = 1;
-
+	//how many lines we have 
 	const int rows = pad * 2 + 3;
-
+	//how many symbols in 1 line
 	const std::string::size_type cols = greeting.size() + pad * 2 + 2;
 
 	std::cout << std::endl;
-
-	
-
+	// i know about for, but now use while cycle for output
 	int r = 0;
-
 	while (r != rows)
 	{
 		std::string::size_type c = 0;
